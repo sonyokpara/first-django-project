57773c7ef6aa70b562700bfca6b987761651c2b1
sonyokpara <sony.okpara@yahoo.com>
2022-10-08 18:44:30 +0100

    Add account app with user authenticatioin

---
+++ mysite/account/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('create-user/', views.CreateUser.as_view(), name='account-create')
+]
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -40,7 +40,17 @@ INSTALLED_APPS = [
 
     # my apps
     'blog',
+    'account',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
+
+REST_FRAMEWORK = {
+
+    'DEFAULT_AUTHENTICATION_CLASSES': {
+        'rest_framework.authentication.TokenAuthentication'
+    }
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
